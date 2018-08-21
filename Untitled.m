e231a036356852b43a100941d22f71780237ccc8
NeedsSoySauce <30617834+NeedsSoySauce@users.noreply.github.com>
2018-08-22 06:56:38 +1200

    SelectKMeansStarted

---
+++ Untitled.m
@@ -0,0 +1,11 @@
+function pos_array = SelectKRandomPoints(array, K)
+    % SelectKRandomPoints finds K random points an a given array
+    % Input(s):     array       = a 3D array 
+    %               K           = how many random points to find
+    % Output(s):    pos_array   = 2D array containing the subscript index of
+    %                             the randomly selected elements in 'array'
+    % Author: Feras Albaroudi
+    
+    
+
+end
